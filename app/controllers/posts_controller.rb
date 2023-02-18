96a779f4bd8a2dd45aae02011d41d46aa0b6283d
Shraddha Rao <shraddharaom@gmail.com>
2023-02-17 22:42:45 -0600

    commit tree 93d3c85f78edf1ec5d300031f4cbe5a4cd2ae39a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,18 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    post.update!(post_params)
+    render json: post, status: :ok
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   private
-
   def post_params
     params.permit(:category, :content, :title)
   end
